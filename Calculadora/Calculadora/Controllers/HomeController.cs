96588531a5cbb71a489a4c14d2dcbbae7682033c
jcnpereira <casimiro@ipt.pt>
2022-03-14 22:23:48 +0000

    processamento dos algarismos

--- Calculadora/Calculadora/Controllers/HomeController.cs
+++ Calculadora/Calculadora/Controllers/HomeController.cs
@@ -4,30 +4,70 @@ using Microsoft.AspNetCore.Mvc;
 
 using System.Diagnostics;
 
-namespace Calculadora.Controllers
-{
-   public class HomeController : Controller
-   {
+namespace Calculadora.Controllers {
+   public class HomeController : Controller {
       private readonly ILogger<HomeController> _logger;
 
-      public HomeController(ILogger<HomeController> logger)
-      {
+      public HomeController(ILogger<HomeController> logger) {
          _logger = logger;
       }
 
-      public IActionResult Index()
-      {
+
+
+      [HttpGet]  // esta anotação é facultativa
+      public IActionResult Index() {
+
+         // inicializar a calculadora
+         ViewBag.Visor = "0";
+
+
          return View();
       }
 
-      public IActionResult Privacy()
-      {
+
+
+      [HttpPost]  // mas, esta anotação já é obrigatória, para o método 'escutar' o HTTP POST
+      public IActionResult Index(string botao, string visor) {
+
+         switch (botao) {
+            case "0":
+            case "1":
+            case "2":
+            case "3":
+            case "4":
+            case "5":
+            case "6":
+            case "7":
+            case "8":
+            case "9":
+               // atribuir ao VISOR o algarismo selecionado
+               if (visor != "0") visor = visor + botao;
+               else { visor = botao; }
+
+
+
+
+               break;
+
+         }
+
+         // preparar dados a serem enviados para a View
+         ViewBag.Visor = visor;
+
+
+         return View();
+      }
+
+
+
+
+
+      public IActionResult Privacy() {
          return View();
       }
 
       [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-      public IActionResult Error()
-      {
+      public IActionResult Error() {
          return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
       }
    }
